84a8647959e84bf506f245efc7044a31a85fe1b9
dhruvmehtaaa <dhruvmehtaalt@gmail.com>
2023-10-07 09:49:08 +0530

    add app main loop and a simple timer

--- tui/src/app/mod.rs
+++ tui/src/app/mod.rs
@@ -1,4 +1,7 @@
+use std::{sync::Arc, time::Duration};
+
 use crossterm::event::{KeyCode, KeyEvent, KeyEventKind, KeyModifiers};
+use tokio::sync::{broadcast, RwLock};
 
 use self::termination::{Interrupted, Terminator};
 
@@ -15,6 +18,7 @@ pub(crate) struct App {
     pub(crate) cursor_position: usize,
     pub(crate) input_mode: InputMode,
     pub(crate) messages: Vec<String>,
+    pub(crate) timer: usize,
 }
 
 impl App {
@@ -25,6 +29,7 @@ impl App {
             input_mode: InputMode::Normal,
             messages: Vec::new(),
             cursor_position: 0,
+            timer: 0,
         }
     }
 
@@ -63,6 +68,10 @@ impl App {
             },
             _ => {}
         }
+    }
+
+    fn increment_timer(&mut self) {
+        self.timer += 1;
     }
 
     fn move_cursor_left(&mut self) {
@@ -109,4 +118,28 @@ impl App {
         self.input.clear();
         self.reset_cursor();
     }
+}
+
+pub(crate) async fn main_loop(
+    mut interrupt_rx: broadcast::Receiver<Interrupted>,
+    app: Arc<RwLock<App>>,
+) -> anyhow::Result<Interrupted> {
+    let mut ticker = tokio::time::interval(Duration::from_secs(1));
+
+    let result = loop {
+        tokio::select! {
+            // Tick to terminate the select every N milliseconds
+            _ = ticker.tick() => {
+                let mut app = app.write().await;
+
+                app.increment_timer();
+            },
+            // Catch and handle interrupt signal to gracefully shutdown
+            Ok(interrupted) = interrupt_rx.recv() => {
+                break interrupted;
+            }
+        }
+    };
+
+    Ok(result)
 }
